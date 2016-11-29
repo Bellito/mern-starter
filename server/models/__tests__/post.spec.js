38d1851bf5531dd742573c8172d59c4181cc8a10
Adrien ETIENNE <a.etienne92@gmail.com>
2016-11-29 14:27:08 -0500

    fix/test deepEqual to is

--- server/models/__tests__/post.spec.js
+++ server/models/__tests__/post.spec.js
@@ -30,7 +30,7 @@ test.serial('Should correctly give number of Posts', async t => {
     .set('Accept', 'application/json');
 
   t.is(res.status, 200);
-  t.deepEqual(posts.length, res.body.posts.length);
+  t.is(posts.length, res.body.posts.length);
 });
 
 test.serial('Should send correct data when queried against a cuid', async t => {
